700da0cf60447c68aa6c942ff1fd34662f6fbd1f
vitusbt <vitusbt@gmail.com>
2024-01-11 16:21:11 +0100

    pl initial commit test

--- src/models/model.py
+++ src/models/model.py
@@ -1,223 +1,266 @@
+from typing import Any
+#from pytorch_lightning.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
 import torch.nn as nn
+from torch import optim
 import math
 import torch.utils.model_zoo as model_zoo
 
+from pytorch_lightning import LightningModule
+import timm
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152']
+TEST_VAR = 1
 
-model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-}
+def TEST_FUN(x):
+    return x+1
 
+class ResNetModel(LightningModule):
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-class Normalize(nn.Module):
-
-    def __init__(self, power=2):
-        super(Normalize, self).__init__()
-        self.power = power
+    def __init__(self, model_name='resnet18', lr=1e-3):
+        super.__init__()
+        self.net = timm.create_model(model_name=model_name, pretrained=True,num_classes=2)
+        self.lossfunc = nn.CrossEntropyLoss()
 
     def forward(self, x):
-        norm = x.pow(self.power).sum(1, keepdim=True).pow(1. / self.power)
-        out = x.div(norm)
-        return out
+        return self.net(x)
     
-class BasicBlock(nn.Module):
-    expansion = 1
+    def training_step(self, batch, batch_idx):
+        data, target = batch
+        preds = self(data)
+        loss = self.lossfunc(preds, target)
+        acc = (target == preds.argmax(dim=-1)).float().mean()
+        self.log('train_loss', loss)
+        self.log('train_acc', acc)
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        data, target = batch
+        preds = self(data)
+        loss = self.lossfunc(preds, target)
+        acc = (target == preds.argmax(dim=-1)).float().mean()
+        self.log('val_loss', loss)
+        self.log('val_acc', acc)
 
-    def forward(self, x):
-        residual = x
+    
+    def configure_optimizers(self):
+        return optim.AdamW(self.parameters(), lr=self.lr)
+    
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+# __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+#            'resnet152']
 
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * 4)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
+# model_urls = {
+#     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+#     'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+#     'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+#     'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+#     'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+# }
 
 
-class ResNet(nn.Module):
+# def conv3x3(in_planes, out_planes, stride=1):
+#     """3x3 convolution with padding"""
+#     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+#                      padding=1, bias=False)
 
-    def __init__(self, block, layers, low_dim=2):
-        self.inplanes = 64
-        super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, low_dim)
-        self.l2norm = Normalize(2)
+# class Normalize(nn.Module):
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+#     def __init__(self, power=2):
+#         super(Normalize, self).__init__()
+#         self.power = power
 
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
+#     def forward(self, x):
+#         norm = x.pow(self.power).sum(1, keepdim=True).pow(1. / self.power)
+#         out = x.div(norm)
+#         return out
+    
+# class BasicBlock(nn.Module):
+#     expansion = 1
 
-        layers = [block(self.inplanes, planes, stride, downsample)]
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
+#     def __init__(self, inplanes, planes, stride=1, downsample=None):
+#         super(BasicBlock, self).__init__()
+#         self.conv1 = conv3x3(inplanes, planes, stride)
+#         self.bn1 = nn.BatchNorm2d(planes)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.conv2 = conv3x3(planes, planes)
+#         self.bn2 = nn.BatchNorm2d(planes)
+#         self.downsample = downsample
+#         self.stride = stride
 
-        return nn.Sequential(*layers)
+#     def forward(self, x):
+#         residual = x
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+#         out = self.conv1(x)
+#         out = self.bn1(out)
+#         out = self.relu(out)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+#         out = self.conv2(out)
+#         out = self.bn2(out)
 
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        x = self.l2norm(x)
+#         if self.downsample is not None:
+#             residual = self.downsample(x)
 
-        return x
+#         out += residual
+#         out = self.relu(out)
+
+#         return out
 
 
-def resnet18(pretrained=True, **kwargs):
-    """Constructs a ResNet-18 model.
+# class Bottleneck(nn.Module):
+#     expansion = 4
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
-    return model
+#     def __init__(self, inplanes, planes, stride=1, downsample=None):
+#         super(Bottleneck, self).__init__()
+#         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(planes)
+#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+#                                padding=1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(planes)
+#         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+#         self.bn3 = nn.BatchNorm2d(planes * 4)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.downsample = downsample
+#         self.stride = stride
 
+#     def forward(self, x):
+#         residual = x
 
-def resnet34(pretrained=True, **kwargs):
-    """Constructs a ResNet-34 model.
+#         out = self.conv1(x)
+#         out = self.bn1(out)
+#         out = self.relu(out)
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
-    return model
+#         out = self.conv2(out)
+#         out = self.bn2(out)
+#         out = self.relu(out)
 
+#         out = self.conv3(out)
+#         out = self.bn3(out)
 
-def resnet50(pretrained=True, **kwargs):
-    """Constructs a ResNet-50 model.
+#         if self.downsample is not None:
+#             residual = self.downsample(x)
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
-    return model
+#         out += residual
+#         out = self.relu(out)
 
-
-def resnet101(pretrained=True, **kwargs):
-    """Constructs a ResNet-101 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
-    return model
+#         return out
 
 
-def resnet152(pretrained=True, **kwargs):
-    """Constructs a ResNet-152 model.
+# class ResNet(nn.Module):
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
-    return model
+#     def __init__(self, block, layers, low_dim=2):
+#         self.inplanes = 64
+#         super(ResNet, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+#                                bias=False)
+#         self.bn1 = nn.BatchNorm2d(64)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+#         self.layer1 = self._make_layer(block, 64, layers[0])
+#         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+#         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+#         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+#         self.avgpool = nn.AvgPool2d(7, stride=1)
+#         self.fc = nn.Linear(512 * block.expansion, low_dim)
+#         self.l2norm = Normalize(2)
+
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+#                 m.weight.data.normal_(0, math.sqrt(2. / n))
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 m.weight.data.fill_(1)
+#                 m.bias.data.zero_()
+
+#     def _make_layer(self, block, planes, blocks, stride=1):
+#         downsample = None
+#         if stride != 1 or self.inplanes != planes * block.expansion:
+#             downsample = nn.Sequential(
+#                 nn.Conv2d(self.inplanes, planes * block.expansion,
+#                           kernel_size=1, stride=stride, bias=False),
+#                 nn.BatchNorm2d(planes * block.expansion),
+#             )
+
+#         layers = [block(self.inplanes, planes, stride, downsample)]
+#         self.inplanes = planes * block.expansion
+#         for i in range(1, blocks):
+#             layers.append(block(self.inplanes, planes))
+
+#         return nn.Sequential(*layers)
+
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.bn1(x)
+#         x = self.relu(x)
+#         x = self.maxpool(x)
+
+#         x = self.layer1(x)
+#         x = self.layer2(x)
+#         x = self.layer3(x)
+#         x = self.layer4(x)
+
+#         x = self.avgpool(x)
+#         x = x.view(x.size(0), -1)
+#         x = self.fc(x)
+#         x = self.l2norm(x)
+
+#         return x
+
+
+# def resnet18(pretrained=True, **kwargs):
+#     """Constructs a ResNet-18 model.
+
+#     Args:
+#         pretrained (bool): If True, returns a model pre-trained on ImageNet
+#     """
+#     model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+#     if pretrained:
+#         model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+#     return model
+
+
+# def resnet34(pretrained=True, **kwargs):
+#     """Constructs a ResNet-34 model.
+
+#     Args:
+#         pretrained (bool): If True, returns a model pre-trained on ImageNet
+#     """
+#     model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+#     if pretrained:
+#         model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
+#     return model
+
+
+# def resnet50(pretrained=True, **kwargs):
+#     """Constructs a ResNet-50 model.
+
+#     Args:
+#         pretrained (bool): If True, returns a model pre-trained on ImageNet
+#     """
+#     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+#     if pretrained:
+#         model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+#     return model
+
+
+# def resnet101(pretrained=True, **kwargs):
+#     """Constructs a ResNet-101 model.
+
+#     Args:
+#         pretrained (bool): If True, returns a model pre-trained on ImageNet
+#     """
+#     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+#     if pretrained:
+#         model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
+#     return model
+
+
+# def resnet152(pretrained=True, **kwargs):
+#     """Constructs a ResNet-152 model.
+
+#     Args:
+#         pretrained (bool): If True, returns a model pre-trained on ImageNet
+#     """
+#     model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+#     if pretrained:
+#         model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
+#     return model
--- src/train_model.py
+++ src/train_model.py
@@ -10,100 +10,153 @@ import argparse
 import yaml
 from torch.utils.tensorboard import SummaryWriter
 logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
-from src.models.model import resnet18 as ResNet
-import wandb
+
 from src.data.make_dataset import get_dataloaders
+from src.models.model import resnet18 as ResNet
+print(ResNet)
+from src.models.model import TEST_VAR
+from src.models.model import TEST_FUN
+from src.models.model import ResNetModel
+import wandb
 import timm
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+
 
 def create_result_folders(experiment_name):
     os.makedirs("results", exist_ok=True)
     os.makedirs(os.path.join("results", experiment_name), exist_ok=True)
 
 def train(config = None):
-    with wandb.init(config=config, 
-                    project="MLOps_Project",
-                    entity="luxonice",):
-        print(wandb.config)
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
-        print(f"Model will run on {device}")
 
-        batch_size = wandb.config.batch_size
-        lr = wandb.config.lr
-        num_epochs = wandb.config.num_epochs
-        time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    batch_size = config.batch_size
+    lr = config.lr
+    num_epochs = config.num_epochs
 
-        # split config_path
-        if config is not None:
-            experiment_name = config.split('/')[-1].split('.')[0]
-            logger = SummaryWriter(os.path.join("runs", experiment_name, time_stamp))
-        else:
-            experiment_name = "sweep"
-            logger = SummaryWriter(os.path.join("sweeps", experiment_name, time_stamp))
+    model = ResNetModel('resnet18', lr=lr)
+    
+    # split config_path
+    if config is not None:
+        experiment_name = config.split('/')[-1].split('.')[0]
+        logger = SummaryWriter(os.path.join("runs", experiment_name, time_stamp))
+    else:
+        experiment_name = "sweep"
+        logger = SummaryWriter(os.path.join("sweeps", experiment_name, time_stamp))
+
+    create_result_folders(os.path.join(experiment_name, time_stamp))
+    trainloader, valloader, _ = get_dataloaders(batch_size)
+    
+    model_path = os.path.join("models", experiment_name, time_stamp)
+    os.makedirs(model_path, exist_ok=True)
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=model_path, monitor="val_loss", mode="min"
+    )
+    early_stopping_callback = EarlyStopping(
+        monitor="val_loss", patience=3, verbose=True, mode="min"
+    )
+    wandb_logger = WandbLogger(config=config, project="MLOps_Project", entity="luxonice")
+
+    trainer = Trainer(
+        callbacks=[checkpoint_callback, early_stopping_callback],
+        accelerator="auto",
+        max_epochs=config.num_epochs,
+        logger=wandb_logger,
+
+        )
+
+    trainer.fit(model, trainloader, valloader)
+
+
+    
+
+
+
+    # with wandb.init(config=config, 
+    #                 project="MLOps_Project",
+    #                 entity="luxonice",):
+    #     print(wandb.config)
+    #     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
+    #     print(f"Model will run on {device}")
+
+    #     batch_size = wandb.config.batch_size
+    #     lr = wandb.config.lr
+    #     num_epochs = wandb.config.num_epochs
+    #     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    #     # split config_path
+    #     if config is not None:
+    #         experiment_name = config.split('/')[-1].split('.')[0]
+    #         logger = SummaryWriter(os.path.join("runs", experiment_name, time_stamp))
+    #     else:
+    #         experiment_name = "sweep"
+    #         logger = SummaryWriter(os.path.join("sweeps", experiment_name, time_stamp))
         
-        create_result_folders(os.path.join(experiment_name, time_stamp))
-        trainloader, valloader, _ = get_dataloaders(batch_size)
+    #     create_result_folders(os.path.join(experiment_name, time_stamp))
+    #     trainloader, valloader, _ = get_dataloaders(batch_size)
 
-        #model = ResNet()
-        model = timm.create_model('resnet18', pretrained=True,num_classes=2)
-        optimizer = optim.AdamW(model.parameters(), lr=lr)
-        lossfunc = torch.nn.CrossEntropyLoss()
+    #     #model = ResNet()
+    #     model = timm.create_model('resnet18', pretrained=True,num_classes=2)
+    #     optimizer = optim.AdamW(model.parameters(), lr=lr)
+    #     lossfunc = torch.nn.CrossEntropyLoss()
 
-        l = len(trainloader)
-        val_loss_current = np.inf
+    #     l = len(trainloader)
+    #     val_loss_current = np.inf
 
-        for epoch in range(1, num_epochs + 1):
-            logging.info(f"Starting epoch {epoch}:")
-            num_batches = tqdm(trainloader)
-            model.train()
+    #     for epoch in range(1, num_epochs + 1):
+    #         logging.info(f"Starting epoch {epoch}:")
+    #         num_batches = tqdm(trainloader)
+    #         model.train()
 
-            for i, (image, label) in enumerate(num_batches):
-                image = image.to(device)
-                label = label.to(device)
+    #         for i, (image, label) in enumerate(num_batches):
+    #             image = image.to(device)
+    #             label = label.to(device)
 
-                with torch.cuda.amp.autocast():
-                    prediction = model(image)
-                    loss = lossfunc(prediction,label)
+    #             with torch.cuda.amp.autocast():
+    #                 prediction = model(image)
+    #                 loss = lossfunc(prediction,label)
                 
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+    #             optimizer.zero_grad()
+    #             loss.backward()
+    #             optimizer.step()
 
-                wandb.log({'train loss': loss})
+    #             wandb.log({'train loss': loss})
 
-                num_batches.set_postfix(MSE=loss.item())
-                logger.add_scalar("train MSE", loss.item(), global_step=epoch * l + i)
-            # time stamp
+    #             num_batches.set_postfix(MSE=loss.item())
+    #             logger.add_scalar("train MSE", loss.item(), global_step=epoch * l + i)
+    #         # time stamp
             
-            num_batches_val = tqdm(valloader)
-            model.eval()
-            # pick a random integer between 0 and len(valloader)
-            random_idx = random.randint(0, len(valloader)-1)
-            val_loss = 0
-            for i, (image, label) in enumerate(num_batches_val):
-                image = image.to(device)
-                label = label.to(device)
+    #         num_batches_val = tqdm(valloader)
+    #         model.eval()
+    #         # pick a random integer between 0 and len(valloader)
+    #         random_idx = random.randint(0, len(valloader)-1)
+    #         val_loss = 0
+    #         for i, (image, label) in enumerate(num_batches_val):
+    #             image = image.to(device)
+    #             label = label.to(device)
                 
-                with torch.cuda.amp.autocast():
-                    prediction = model(image) # prediction
-                    val_loss += lossfunc(prediction,label).item() # loss between noise and predicted noise
+    #             with torch.cuda.amp.autocast():
+    #                 prediction = model(image) # prediction
+    #                 val_loss += lossfunc(prediction,label).item() # loss between noise and predicted noise
 
-                if i == random_idx:
-                    random_image = image
-                    random_label = label
-                    if random_image.shape[0] > 4:
-                        random_image = random_image[:4]
-                        random_label = random_label[:4]
+    #             if i == random_idx:
+    #                 random_image = image
+    #                 random_label = label
+    #                 if random_image.shape[0] > 4:
+    #                     random_image = random_image[:4]
+    #                     random_label = random_label[:4]
 
-            val_loss /= len(valloader)
+    #         val_loss /= len(valloader)
 
-            wandb.log({'val loss': val_loss})
+    #         wandb.log({'val loss': val_loss})
 
-            logger.add_scalar("val MSE", val_loss, global_step=epoch)
+    #         logger.add_scalar("val MSE", val_loss, global_step=epoch)
             
-            if val_loss < val_loss_current:
-                val_loss_current = val_loss
-                os.makedirs(os.path.join("models", experiment_name, time_stamp), exist_ok=True)
-                torch.save(model.state_dict(), os.path.join("models", experiment_name, time_stamp, f"weights-{epoch}.pt"))
+    #         if val_loss < val_loss_current:
+    #             val_loss_current = val_loss
+    #             os.makedirs(os.path.join("models", experiment_name, time_stamp), exist_ok=True)
+    #             torch.save(model.state_dict(), os.path.join("models", experiment_name, time_stamp, f"weights-{epoch}.pt"))
 
 
 if __name__ == '__main__':
