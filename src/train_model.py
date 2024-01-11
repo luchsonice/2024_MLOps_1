31db66ba3af3ab5dd375ce0b8ae653e5cf529a92
luchsonice <s194369@student.dtu.dk>
2024-01-11 10:20:59 +0100

    continue work

--- src/train_model.py
+++ src/train_model.py
@@ -10,7 +10,7 @@ import argparse
 import yaml
 from torch.utils.tensorboard import SummaryWriter
 logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
-from src.models.model import resnet
+from src.models.model import resnet18 as ResNet
 import wandb
 from src.data.make_dataset import get_dataloaders
 
@@ -18,24 +18,6 @@ from src.data.make_dataset import get_dataloaders
 def create_result_folders(experiment_name):
     os.makedirs("results", exist_ok=True)
     os.makedirs(os.path.join("results", experiment_name), exist_ok=True)
-
-def freeze_pretrained_layers(model):
-    '''Freeze all layers except the last layer(fc or classifier)'''
-    for param in model.parameters():
-            param.requires_grad = False
-    model.fc.weight.requires_grad = True
-    model.fc.bias.requires_grad = True
-
-def debarcle_layers(model, num_debarcle):
-    '''Debarcle From the last [-1]layer to the [-num_debarcle] layers, 
-    approximately(for there is Conv2d which has only weight parameter)'''
-    param_name = [name for name,_ in model.named_parameters()]
-    num_debarcle *= 2
-    param_debarcle = param_name[-num_debarcle:]
-    if param_debarcle[0].split('.')[-1] == 'bias':
-        param_debarcle = param_name[-(num_debarcle + 1):]
-    for name, param in model.named_parameters():
-        param.requires_grad = True if name in param_debarcle else False
 
 def train(config = None):
     with wandb.init(config=config, 
@@ -62,7 +44,7 @@ def train(config = None):
         create_result_folders(os.path.join(experiment_name, time_stamp))
         trainloader, _, valloader = get_dataloaders(batch_size)
 
-        model = resnet()
+        model = ResNet()
         optimizer = optim.AdamW(model.parameters(), lr=lr)
         lossfunc = torch.nn.MSELoss()
 
