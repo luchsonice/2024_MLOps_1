9af9dc3b570516a525f540104d525297c502b6c7
vitusbt <vitusbt@gmail.com>
2024-01-12 09:11:02 +0100

    pytorch lightning works now

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import sys
 import os
 import random
 import torch
@@ -10,18 +9,10 @@ import datetime
 import argparse
 import yaml
 import pytorch_lightning
-from torch.utils.tensorboard import SummaryWriter
+#from torch.utils.tensorboard import SummaryWriter
 logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
 
-print(sys.path)
-
-#from src.data.make_dataset import *
-#from src.models.model import *
 from src.data.make_dataset import get_dataloaders
-#from src.data.make_dataset import TEST_DATA_VAR
-from src.models.model import resnet34
-#from src.models.model import TEST_VAR
-#from src.models.model import TEST_FUN
 from src.models.model import ResNetModel
 
 import wandb
@@ -40,22 +31,19 @@ def train(config_file = None):
     with open(config_file, 'r') as file:
         config = yaml.safe_load(file)
     
-    print(config)
-    print(type(config))
-    batch_size = config['batch_size']
-    lr = config['lr']
-    num_epochs = config['num_epochs']
+    hyperparameters = config['hyperparameters']
+    batch_size = hyperparameters['batch_size']
+    lr = hyperparameters['lr']
+    num_epochs = hyperparameters['num_epochs']
     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     model = ResNetModel('resnet18', lr=lr)
     
     # split config_path
-    if config is not None:
-        experiment_name = config.split('/')[-1].split('.')[0]
-        logger = SummaryWriter(os.path.join("runs", experiment_name, time_stamp))
+    if config_file is not None:
+        experiment_name = config_file.split('/')[-1].split('.')[0]
     else:
         experiment_name = "sweep"
-        logger = SummaryWriter(os.path.join("sweeps", experiment_name, time_stamp))
 
     create_result_folders(os.path.join(experiment_name, time_stamp))
     trainloader, valloader, _ = get_dataloaders(batch_size)
@@ -73,102 +61,12 @@ def train(config_file = None):
     trainer = Trainer(
         callbacks=[checkpoint_callback, early_stopping_callback],
         accelerator="auto",
-        max_epochs=config.num_epochs,
+        max_epochs=num_epochs,
         logger=wandb_logger,
-
+        log_every_n_steps=1
         )
 
     trainer.fit(model, trainloader, valloader)
-
-
-    
-
-
-
-    # with wandb.init(config=config, 
-    #                 project="MLOps_Project",
-    #                 entity="luxonice",):
-    #     print(wandb.config)
-    #     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
-    #     print(f"Model will run on {device}")
-
-    #     batch_size = wandb.config.batch_size
-    #     lr = wandb.config.lr
-    #     num_epochs = wandb.config.num_epochs
-    #     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-    #     # split config_path
-    #     if config is not None:
-    #         experiment_name = config.split('/')[-1].split('.')[0]
-    #         logger = SummaryWriter(os.path.join("runs", experiment_name, time_stamp))
-    #     else:
-    #         experiment_name = "sweep"
-    #         logger = SummaryWriter(os.path.join("sweeps", experiment_name, time_stamp))
-        
-    #     create_result_folders(os.path.join(experiment_name, time_stamp))
-    #     trainloader, valloader, _ = get_dataloaders(batch_size)
-
-    #     #model = ResNet()
-    #     model = timm.create_model('resnet18', pretrained=True,num_classes=2)
-    #     optimizer = optim.AdamW(model.parameters(), lr=lr)
-    #     lossfunc = torch.nn.CrossEntropyLoss()
-
-    #     l = len(trainloader)
-    #     val_loss_current = np.inf
-
-    #     for epoch in range(1, num_epochs + 1):
-    #         logging.info(f"Starting epoch {epoch}:")
-    #         num_batches = tqdm(trainloader)
-    #         model.train()
-
-    #         for i, (image, label) in enumerate(num_batches):
-    #             image = image.to(device)
-    #             label = label.to(device)
-
-    #             with torch.cuda.amp.autocast():
-    #                 prediction = model(image)
-    #                 loss = lossfunc(prediction,label)
-                
-    #             optimizer.zero_grad()
-    #             loss.backward()
-    #             optimizer.step()
-
-    #             wandb.log({'train loss': loss})
-
-    #             num_batches.set_postfix(MSE=loss.item())
-    #             logger.add_scalar("train MSE", loss.item(), global_step=epoch * l + i)
-    #         # time stamp
-            
-    #         num_batches_val = tqdm(valloader)
-    #         model.eval()
-    #         # pick a random integer between 0 and len(valloader)
-    #         random_idx = random.randint(0, len(valloader)-1)
-    #         val_loss = 0
-    #         for i, (image, label) in enumerate(num_batches_val):
-    #             image = image.to(device)
-    #             label = label.to(device)
-                
-    #             with torch.cuda.amp.autocast():
-    #                 prediction = model(image) # prediction
-    #                 val_loss += lossfunc(prediction,label).item() # loss between noise and predicted noise
-
-    #             if i == random_idx:
-    #                 random_image = image
-    #                 random_label = label
-    #                 if random_image.shape[0] > 4:
-    #                     random_image = random_image[:4]
-    #                     random_label = random_label[:4]
-
-    #         val_loss /= len(valloader)
-
-    #         wandb.log({'val loss': val_loss})
-
-    #         logger.add_scalar("val MSE", val_loss, global_step=epoch)
-            
-    #         if val_loss < val_loss_current:
-    #             val_loss_current = val_loss
-    #             os.makedirs(os.path.join("models", experiment_name, time_stamp), exist_ok=True)
-    #             torch.save(model.state_dict(), os.path.join("models", experiment_name, time_stamp, f"weights-{epoch}.pt"))
 
 
 if __name__ == '__main__':
