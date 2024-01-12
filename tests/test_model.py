1e1f10243b5797d264187b5639db3ffcf751bd0a
vitusbt <vitusbt@gmail.com>
2024-01-12 11:07:22 +0100

    unittests for data and model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,14 @@
-def test_model():
+import os
+import pytest
+import torch
+
+from src.models.model import ResNetModel
+
+@pytest.mark.parametrize('bs', [1, 23, 64])
+def test_model(bs):
     # Test if correct output given input
-    pass
+    model = ResNetModel('resnet18')
+    input = torch.randn(size=(bs, 3, 250, 250))
+    output = model(input)
+    assert output.shape == (bs, 2)
+
