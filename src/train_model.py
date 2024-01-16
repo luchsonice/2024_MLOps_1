2167a0f7e4f8c1cf1d31e7ba328053fa39b481c1
luchsonice <s194369@student.dtu.dk>
2024-01-16 14:43:32 +0100

    added make sweep

--- src/train_model.py
+++ src/train_model.py
@@ -9,70 +9,63 @@ from src.models.model import ResNetModel
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+import argparse
+import wandb
 
-def create_result_folders(experiment_name):
+def create_result_folders(config_name):
     os.makedirs("results", exist_ok=True)
-    os.makedirs(os.path.join("results", experiment_name), exist_ok=True)
+    os.makedirs(os.path.join("results", config_name), exist_ok=True)
 
-def train(config = None, config_name = None):
-    # Read hyperparameters
-    batch_size = config['batch_size']
-    lr = config['lr']
-    num_epochs = config['num_epochs']
+def train(config = None):
+    with wandb.init(config=config, project="MLOps_Project", entity="luxonice"):
+        # If called by wandb.agent, as below,
+        # this config will be set by Sweep Controller
+        config = wandb.config
 
-    # Create model
-    model = ResNetModel('resnet18', lr=lr)
+        lr = wandb.config.lr
+        batch_size = wandb.config.batch_size
+        num_epochs = wandb.config.num_epochs
+        config_name = wandb.config.name
 
-    # split config_path
-    if config_name is not None:
-        experiment_name = config_name
-    else:
-        experiment_name = "sweep"
+        # Create model
+        model = ResNetModel('resnet18', lr=lr)
 
-    # Create results folder
-    time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    create_result_folders(os.path.join(experiment_name, time_stamp))
+        # Create results folder
+        time_stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        create_result_folders(os.path.join(config_name, time_stamp))
 
-    # Create models folder
-    model_path = os.path.join("models", experiment_name, time_stamp)
-    os.makedirs(model_path, exist_ok=True)
+        # Create models folder
+        model_path = os.path.join("models", config_name, time_stamp)
+        os.makedirs(model_path, exist_ok=True)
 
-    # Get dataloaders
-    trainloader, valloader, _ = get_dataloaders(batch_size)
+        # Get dataloaders
+        trainloader, valloader, _ = get_dataloaders(batch_size)
 
-    # Create callbacks
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=model_path, monitor="val_loss", mode="min", save_top_k=1
-    )
-    early_stopping_callback = EarlyStopping(
-        monitor="val_loss", patience=3, verbose=True, mode="min"
-    )
-    # Create Wandb logger
-    wandb_logger = WandbLogger(config=config, project="MLOps_Project", entity="luxonice")
-
-    # Create trainer
-    trainer = Trainer(
-        callbacks=[checkpoint_callback, early_stopping_callback],
-        accelerator="auto",
-        max_epochs=num_epochs,
-        logger=wandb_logger,
-        log_every_n_steps=1
+        # Create callbacks
+        checkpoint_callback = ModelCheckpoint(
+            dirpath=model_path, monitor="val_loss", mode="min", save_top_k=1
         )
+        early_stopping_callback = EarlyStopping(
+            monitor="val_loss", patience=3, verbose=True, mode="min"
+        )
+        # Create Wandb logger
+        wandb_logger = WandbLogger(config=config, project="MLOps_Project", entity="luxonice")
 
-    # Train the model
-    trainer.fit(model, trainloader, valloader)
+        # Create trainer
+        trainer = Trainer(
+            callbacks=[checkpoint_callback, early_stopping_callback],
+            accelerator="auto",
+            max_epochs=num_epochs,
+            logger=wandb_logger,
+            log_every_n_steps=1
+            )
 
-# Uses hydra to load the config file
-@hydra.main(version_base=None, config_path=".." + os.sep + "configs", config_name="config")
-def main(cfg : DictConfig) -> None:
-    # Store name from config file
-    config_name = cfg.name
-    # Store hyperparameters from config file as a dict
-    config = dict(cfg.hyperparameters)
-    # Train the model with the given hyperparameters
-    train(config, config_name)
-
-
+        # Train the model
+        trainer.fit(model, trainloader, valloader)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='configs/config.yaml')
+    args = parser.parse_args()
+    config = args.config
+    train(config)
