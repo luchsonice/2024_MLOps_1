0bed9c22779ab2ae6d1abc6380cfd8fb5e192d1e
luchsonice <s194369@student.dtu.dk>
2024-01-17 14:12:46 +0100

    updated model_path

--- src/models/pickle_model.py
+++ src/models/pickle_model.py
@@ -1,7 +1,7 @@
 import pickle
 from src.models.model import ResNetModel
 
-model_path = "models/config/2024-01-16_12-15-23/epoch=1-step=24.ckpt"
+model_path = "models/config/2024-01-17_12-46-36/epoch=15-step=96.ckpt"
 
 # Load model checkpoint
 model = ResNetModel.load_from_checkpoint(model_path)
