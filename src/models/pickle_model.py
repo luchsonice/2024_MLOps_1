df288b7ccedb8b1767d6956e8e8155708a4c1249
vitusbt <vitusbt@gmail.com>
2024-01-17 15:03:49 +0100

    change pickle

--- src/models/pickle_model.py
+++ src/models/pickle_model.py
@@ -7,4 +7,4 @@ model_path = "models/config/2024-01-17_12-46-36/epoch=15-step=96.ckpt"
 model = ResNetModel.load_from_checkpoint(model_path)
 
 with open('model_to_deploy.pickle', 'wb') as handle:
-    pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(model.net, handle, protocol=pickle.HIGHEST_PROTOCOL)
